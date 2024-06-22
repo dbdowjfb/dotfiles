847f2b30c112c0e1584e2d34b05c7484eab12d1f
dbdo <dbdo>
2024-06-22 11:29:26 -0400

    org capture, org_return_uses_meta_return

--- nvim/lua/plugins/org.lua
+++ nvim/lua/plugins/org.lua
@@ -6,9 +6,16 @@ return {
     -- Setup orgmode
     require('orgmode').setup({
       org_agenda_files = '~/syncthing/DOC/**/*',
-      org_default_notes_file = '~/syncthing/DOC/refile.org',
-    })
-
+      org_default_notes_file = '~/syncthing/DOC/todo.org',
+      org_capture_templates = {
+    --     { t = { description = 'Task', template = '* TODO %?\n  %u' } },
+          n = { description = 'Note', template = '%T \n %?', target = '~/syncthing/DOC/notes.org'}
+    },
+    mappings = {
+      org_return_uses_meta_return = true
+    }
+  })
+    
     -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option add ~org~ to ignore_install
     -- require('nvim-treesitter.configs').setup({
     --   ensure_installed = 'all',
